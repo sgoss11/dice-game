4c6e3b7d782cdb386602a4df48084c3ebf509c28
Sierra Goss <sgoss11@uw.edu>
2022-10-14 16:25:05 -0700

    Updated Heading for winner

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //roll 1
 var roll1 = Math.floor((Math.random() * 6)+1);
 
-var image1 = document.querySelector("img1");
-
 var newImage1 = "images/dice" + roll + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", newImage1);
 
@@ -11,8 +11,17 @@ image1.setAttribute("src", newImage1);
 
 var roll2 = Math.floor((Math.random() * 6)+1);
 
-var image2 = document.querySelector("img2");
-
 var newImage2 = "images/dice" + roll + ".png";
 
+var image2 = document.querySelectorAll("img")[1];
+
 image2.setAttribute("src", newImage2);
+
+//update new heading
+var title = document.querySelect("h1");
+
+if(roll1 > roll2){
+    title.textContent = "Player 1 Wins!";
+}else{
+    title.textContent = "Player 2 Wins!";
+} 
