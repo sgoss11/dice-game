9f68ff308fdd8c74cc090eb7262a8aeb109df57a
Sierra Goss <sgoss11@uw.edu>
2022-10-14 16:15:48 -0700

    Retry

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
-var roll1 = Math.floor((Math.random() * 6) + 1);
+//roll 1
+var roll1 = Math.floor((Math.random() * 6)+1);
 
-if(roll === 1){
-    document.querySelector("img1").setAttribute("src", "images/dice1.png");
-}else if(roll ===2){
-    document.querySelector("img1").setAttribute("src", "images/dice2.png");
-}else if(roll ===3){
-    document.querySelector("img1").setAttribute("src", "images/dice3.png");
-}else if(roll ===4){
-    document.querySelector("img1").setAttribute("src", "images/dice4.png");
-}else if(roll ===5){
-    document.querySelector("img1").setAttribute("src", "images/dice5.png");
-}else if(roll ===6){
-    document.querySelector("img1").setAttribute("src", "images/dice6.png");
-}
+var image1 = document.querySelector("img1");
 
-var roll2 = Math.floor((Math.random() * 6) + 1);
+var newImage1 = "images/dice" + roll + ".png";
 
-if(roll2 === 1){
-    document.querySelector("img2").setAttribute("src", "images/dice1.png");
-}else if(roll2 ===2){
-    document.querySelector("img2").setAttribute("src", "images/dice2.png");
-}else if(roll2 ===3){
-    document.querySelector("img2").setAttribute("src", "images/dice3.png");
-}else if(roll2 ===4){
-    document.querySelector("img2").setAttribute("src", "images/dice4.png");
-}else if(roll2 ===5){
-    document.querySelector("img2").setAttribute("src", "images/dice5.png");
-}else if(roll2 ===6){
-    document.querySelector("img2").setAttribute("src", "images/dice6.png");
-}
+image1.setAttribute("src", newImage1);
 
-if(roll1 > roll2){
-    document.querySelector("h1").textContent = "Player 1 Wins!";
-}else{
-    document.querySelector("h1").textContent = "Player 2 Wins!";
-}
+//roll 2
+
+var roll2 = Math.floor((Math.random() * 6)+1);
+
+var image2 = document.querySelector("img2");
+
+var newImage2 = "images/dice" + roll + ".png";
+
+image2.setAttribute("src", newImage2);
